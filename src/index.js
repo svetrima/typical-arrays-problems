82a26d781358ed35f44da003897d2e5beb549431
Svetrima <svetsmetanina@gmail.com>
2020-02-21 13:10:17 +0300

    feat:complete task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
 
 exports.min = function min (array) {
-  return 0;
+  if(array==null||array.length==0){
+    return 0;
+  }
+  return Math.min(...array);
+  
 }
 
 exports.max = function max (array) {
-  return 0;
+  if(array==null||array.length==0){
+    return 0;
+  }
+  return Math.max(...array);
 }
 
 exports.avg = function avg (array) {
-  return 0;
+   if(array==null||array.length==0){
+      return 0;
+    }
+return (array.reduce((a, b)=>a+b, 0))/array.length;
 }
